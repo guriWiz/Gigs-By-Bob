4e3fe7ccb652aa19ff54bbb70c2af466cbe985c5
Gurvinder Singh <guri.developer97@gmail.com>
2024-06-16 12:57:25 +0530

    Sending the freelancer jobs data from API to discord

--- bob/freelancer.py
+++ bob/freelancer.py
@@ -1,6 +1,11 @@
+import json
 import requests
 import os
 from datetime import datetime, timedelta
+from .logger import LOGGER
+
+BOB_SLEEP_MINS = int(os.getenv("BOB_SLEEP_MINS", 5))
+FR_HOST = os.getenv("FR_HOST")
 
 FR_PJ_HOST = os.getenv("FR_PJ_HOST")
 FR_API_HOST = os.getenv("FR_API_HOST")
@@ -8,51 +13,92 @@ FR_API_TOKEN = os.getenv("FR_API_TOKEN")
 
 class FreelancerBob:
     def __init__(self):
-        self.s_time = round((datetime.now() - timedelta(minutes=5)).timestamp())
+        self.req_headers = {
+            "Content-Type": "application/json",
+            "freelancer-oauth-v1": FR_API_TOKEN
+        }
+
+        self.s_time = round((datetime.now() - timedelta(minutes=BOB_SLEEP_MINS)).timestamp())
         self.e_time = round(datetime.now().timestamp())
 
-    def format_pj(self, pj_obj):        
+        LOGGER.info(f"From time: {self.s_time}")
+        LOGGER.info(f"To time: {self.e_time}")
+
+    def format_user(self, user_obj):
+        u_name = user_obj["username"]
+        
         return {
-            "pj_id": pj_obj["id"],
-            "owner_id": pj_obj["owner_id"],
+            "u_name": u_name,
+            "u_avatar": f"{FR_HOST}{user_obj["avatar"]}",
+            "u_url": f"{FR_HOST}/u/{u_name}"
+        }
+
+    def format_pj(self, pj_obj, users):    
+        pj_id = pj_obj["id"]
+        owner_id = pj_obj["owner_id"]
+
+        user_obj = users[str(owner_id)]
+        user_obj = self.format_user(user_obj)
+
+        return {
+            "pj_id": pj_id,
+            "pj_user": {
+                "u_id": owner_id
+            } | user_obj,
             "pj_title": pj_obj["title"],
+            "pj_desc": pj_obj["description"],
             "pj_url": f"{FR_PJ_HOST}/{pj_obj['seo_url']}",
-            "submit_dt": datetime.fromtimestamp(pj_obj["submitdate"]),
-            "updated_dt": datetime.fromtimestamp(pj_obj["time_updated"]),
             "pj_type": pj_obj["type"],
             "bid_p": pj_obj["bidperiod"],
             "pj_curr": pj_obj["currency"]["sign"],
             "pj_min_bdg": pj_obj["budget"]["minimum"],
             "pj_max_bdg": pj_obj["budget"]["maximum"],
             "bids": pj_obj["bid_stats"]["bid_count"],
-            "avg_bid": round(pj_obj["bid_stats"]["bid_avg"], 2)
+            "avg_bid": round(pj_obj["bid_stats"]["bid_avg"], 2),
+            "submit_dt": datetime.fromtimestamp(pj_obj["submitdate"]),
+            "update_dt": datetime.fromtimestamp(pj_obj["time_updated"])
         }
-
-    def fetch_owner(self):
-        ...
 
     def fetch_projects(self):
         req_url = f"{FR_API_HOST}/projects/0.1/projects/all/"
-        req_header = {
-            "Content-Type": "application/json",
-            "freelancer-oauth-v1": FR_API_TOKEN
-        }
         req_data = {
             "filter": {
                 "jobs": [13],
                 "from_time": self.s_time,
-                "to_time": self.e_time
+                "to_time": self.e_time,
+            },
+            "projection": {
+                "result_projection": {
+                    "users_projection": {
+                        "username": True, "avatar": True, "email": True, "reputation": True, "employer_reputation": True
+                    },
+                    "full_description": True
+                }
             }
         }
 
-        req_obj = requests.post(req_url, headers=req_header, json=req_data)
+        req_obj = requests.post(req_url, headers=self.req_headers, json=req_data)
+        
+        pj_list = []
         req_status = req_obj.status_code
 
         if (req_status >= 200) & (req_status <= 226):
             resp_obj = req_obj.json()
-            projects = resp_obj["result"]["projects"]
 
-            pj_list = []
+            result = resp_obj["result"]
+            projects = result["projects"]
+            users = result["users"]
+            pj_count = result["total_count"]
+
+            LOGGER.info(f"Projects count: {pj_count}")
 
             for pj_obj in projects:
-                pj_list.append(self.format_pj(pj_obj))
+                pj_list.append(self.format_pj(pj_obj, users))
+
+        else:
+            LOGGER.error(f"Projects request failed with status code: {req_status}")
+            LOGGER.error(f"{req_obj.text}")
+
+        LOGGER.info(f"Projects list: {pj_list}")
+        
+        return pj_list
--- bot.py
+++ bot.py
@@ -1,8 +1,65 @@
 from dotenv import load_dotenv
-load_dotenv()
+import os
+import discord
+from discord import Embed, ButtonStyle, Interaction
+from discord.ext import tasks
+from bob.logger import LOGGER
+from discord.ui import View, Button, button
 
+load_dotenv()
 from bob.freelancer import FreelancerBob
 
+class BobBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def send_gig(self, gig, gig_ch):
+        pj_curr = gig["pj_curr"]
+        pj_user = gig["pj_user"]
+
+        gig_msg = ""
+        gig_msg += f"**URL** - {gig['pj_url']} \n\n"
+        gig_msg += f"**Type** - {gig['pj_type']} \n"
+        gig_msg += f"**Bid Period** - {gig['bid_p']} \n"
+        gig_msg += f"**Total Bids** - {gig['bids']} \n"
+        gig_msg += f"**Budget** - {pj_curr} {gig['pj_min_bdg']} to {pj_curr} {gig['pj_max_bdg']} \n"
+        gig_msg += f"**Avg. Bid Price** - {pj_curr} {gig['avg_bid']} \n\n"
+        gig_msg += f"**Description :** \n"
+        gig_msg += f"```{gig['pj_desc']}``` \n"
+        gig_msg += f"**Submitted On** - {gig['submit_dt']} \n"
+        gig_msg += f"**Updated At** - {gig['update_dt']} \n"
+
+        gig_embed = Embed(
+            title=f"**[{gig['pj_id']}] {gig['pj_title']}**",
+            description=gig_msg,
+            color=discord.Color.random()
+        )
+
+        gig_embed.set_author(
+            name=f"[{pj_user["u_id"]}] {pj_user["u_name"]}",
+            url=pj_user["u_url"],
+            icon_url=pj_user["u_avatar"]
+        )
+        # gig_embed.set_footer(text=gfi_data["labels"], icon_url=gfi_data["avatar_url"])
+
+        await gig_ch.send(embed=gig_embed)
+
+    async def on_ready(self):
+        LOGGER.info(f"{bob_client.user} has connected to discord!")
+
+        fb = FreelancerBob()
+        gig_data = fb.fetch_projects()
+
+        gig_ch = bob_client.get_channel(int(GUILD_ID))
+        await gig_ch.purge(limit=1000) # Purges the last 1000 messages from the channel
+
+        for gig in gig_data:
+            await self.send_gig(gig, gig_ch)
+
 if __name__ == '__main__':
-    fb = FreelancerBob()
-    fb.fetch_projects()
+
+    DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+    GUILD_ID = os.getenv("GUILD_ID")
+
+    bob_client = BobBot(intents=discord.Intents.default())
+    bob_client.run(DISCORD_TOKEN)
